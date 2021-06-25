70bf81c52ba793d81af7972e2a207ea5c32c9a1e
KrzysztofCalus <krzysztofcalus@gmail.com>
2021-06-25 21:15:11 +0200

    login and link for resized image

--- upload_image/serializers.py
+++ upload_image/serializers.py
@@ -3,7 +3,17 @@ from upload_image.models import ImageModel
 
 
 class ImageSerializer(serializers.ModelSerializer):
+    small_image = serializers.SerializerMethodField()
+
     class Meta:
         owner = serializers.ReadOnlyField(source='owner.username')
         model = ImageModel
-        fields = ['image']
+        fields = ['image', 'small_image']
+
+    def get_small_image(self, object):
+        """
+        Give additional field in JSON
+        :param object:
+        :return:
+        """
+        return "http://127.0.0.1:8000" + object.image.url
